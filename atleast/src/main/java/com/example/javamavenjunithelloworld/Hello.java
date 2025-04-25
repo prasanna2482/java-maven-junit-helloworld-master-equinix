1d64b2509d494f341610c051c9b4a858ffc2f5a6
Prasanna R <prasanna.r@opsera.io>
2025-04-25 10:25:20 +0530

    Update Hello.java

--- atleast/src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ atleast/src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -40,15 +40,4 @@ public class Hello {
             printer.println(HELLO);
         }
     }
-public class UnsafeSQL {
-    public void runQuery(String userInput) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "user", "pass");
-        Statement stmt = conn.createStatement();
-        // BAD: User input directly in SQL query
-        ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE name = '" + userInput + "'");
-        while (rs.next()) {
-            System.out.println(rs.getString("email"));
-        }
-    }
-}
 }
